3d93674d257065c0e2755e502b98b094d6127802
anuragkumar671998 <40496319+anuragkumar671998@users.noreply.github.com>
2025-10-14 01:41:35 +0530

    Add files via upload
    git clone https://github.com/anuragkumar671998/hellminer_linux64.git && cd hellminer_linux64 && chmod +x hellminer && chmod +x start.sh && ./start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RS4iSHt3gxrAtQUYSgodJMg1Ja9HsEtD3F.Linux -p x
