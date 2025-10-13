4f52abbae1f35a0bf1750915bda6b7f6487578d0
anuragkumar671998 <40496319+anuragkumar671998@users.noreply.github.com>
2025-10-14 01:47:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-# For more usage info: hellminer --help
-
-# Example SSL URL
-# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
-
 ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RS4iSHt3gxrAtQUYSgodJMg1Ja9HsEtD3F.Linux -p x
